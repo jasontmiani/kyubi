4d6901ac58fca3294af8a753887262ccfafcfb51
Jason Miani <jasontmiani@gmail.com>
2022-03-31 00:32:58 -0700

    update site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,9 +49,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `jodie - @lekoarts/gatsby-theme-jodie`,
-        short_name: `jodie`,
-        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+        name: `Kyubi | The Corgi`,
+        short_name: `kyubi`,
+        description: `Site for Kyubi the Corgi`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
